0097bdb5da4747b1e0703ba81fb8d803f705f118
Ford Zamore <fordzamore@Fords-MacBook-Pro-980.local>
2025-03-30 16:10:38 -0400

    Resolved 404 Error via URI update

--- backend/index.php
+++ backend/index.php
@@ -2,12 +2,12 @@
 require __DIR__ . "/inc/bootstrap.php";
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri );
-if ((isset($uri[2]) && $uri[2] != 'user') || !isset($uri[3])) {
+if ((isset($uri[4]) && $uri[4] != 'user') || !isset($uri[5])) {
     header("HTTP/1.1 404 Not Found");
     exit();
 }
 require PROJECT_ROOT_PATH . "/Controller/Api/UserController.php";
 $objFeedController = new UserController();
-$strMethodName = $uri[3] . 'Action';
+$strMethodName = $uri[5] . 'Action';
 $objFeedController->{$strMethodName}();
 ?>
