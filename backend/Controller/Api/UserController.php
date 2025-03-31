224dce8da8db676c3627945a3390dc658bd0ad60
mrichw <mrich@wesleyan.edu>
2025-03-31 14:29:50 -0400

    added createAction to UserController (Issue #100)

--- backend/Controller/Api/UserController.php
+++ backend/Controller/Api/UserController.php
@@ -38,4 +38,50 @@ class UserController extends BaseController
             );
         }
     }
+
+    // Endpoint /user/create - create new user
+    public function createAction()
+    {
+        $strErrorDesc = '';
+        $requestMethod = $_SERVER["REQUEST_METHOD"];
+
+        if (strtoupper($requestMethod) == 'POST') {
+            try {
+                $username = $_POST['username'];
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+                $confirm_password = $_POST['confirm_password'];
+
+                if ($password !== $confirm_password) {
+                    throw new Exception("Passwords do not match.");
+                }
+
+                if (strlen($password) < 10) {
+                    throw new Exception("Password must be at least 10 characters long.");
+                }
+
+                $userModel = new UserModel();
+                $userModel->createUser($username, $email, $password);
+
+                $responseData = json_encode(['success' => true, 'message' => "User created successfully"]);
+            } catch (Error $e) {
+                $strErrorDesc = $e->getMessage();
+                $strErrorHeader = 'HTTP/1.1 400 Bad Request';
+            }
+        } else {
+            $strErrorDesc = 'Method not supported';
+            $strErrorHeader = 'HTTP/1.1 422 Unprocessable Entity';
+        }
+        // send output 
+        if (!$strErrorDesc) {
+            $this->sendOutput(
+                $responseData,
+                array('Content-Type: application/json', 'HTTP/1.1 201 CREATED')
+            );
+        } else {
+            $this->sendOutput(json_encode(array('error' => $strErrorDesc)), 
+                array('Content-Type: application/json', $strErrorHeader)
+            );
+        }
+    }
 }
